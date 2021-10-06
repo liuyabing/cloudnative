21f661853a3fa419ef156cf64ddd3ac6d1760ac0
liuyabing <81155110@qq.com>
2021-10-06 16:36:33 +0800

    commit again

--- lesson3/def.go
+++ lesson3/def.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Queue struct {
+	//定义一个slice
+	queue []int
+	cond  *sync.Cond
+}
+
+// Push 往slice中插入数据
+func (q *Queue) Push(i, num int) {
+	q.cond.L.Lock()
+	defer q.cond.L.Unlock()
+	//在queue这个slice中加入i
+	q.queue = append(q.queue, i)
+	fmt.Printf(">>>>>生产者%d创建了一个消息\n", num)
+
+	q.cond.Broadcast() //通知所有等待的线程
+	//q.cond.Signal()//通知某一个等待的线程
+}
+
+// Pop 从slice中取出数据
+func (q *Queue) Pop(num int) {
+	q.cond.L.Lock()
+	defer q.cond.L.Unlock()
+	//如果slice中没有数据线程就等待
+	for len(q.queue) == 0 {
+		fmt.Println("I'm waiting 1...")
+		q.cond.Wait()
+		fmt.Println("I'm waiting 2...")
+	}
+	//取出slice中第一个数
+	i := q.queue[0]
+	//剩下的slice再赋值给slice
+	q.queue = q.queue[1:]
+	fmt.Printf("<<<<<消费者%d消费了%d\n", num, i)
+}
--- lesson3/main.go
+++ lesson3/main.go
@@ -1,3 +1,35 @@
-package lesson3
+package main
 
+import (
+	"sync"
+	"time"
+)
 
+func main() {
+	//初始化一个Queue这个struct的-指针对象,
+	q := &Queue{
+		queue: []int{},
+		cond:  sync.NewCond(&sync.Mutex{}),
+	}
+
+	//定义5个线程,每个线程通过q对象的Push方法放一个数
+	for num := 1; num < 5; num++ {
+		go func(n int) {
+			q.Push(8, n)
+			time.Sleep(2 * time.Second)
+		}(num)
+	}
+
+	//定义5个线程,每个线程通过q对象的Pop方法取一个数
+	for num := 1; num < 5; num++ {
+		go func(n int) {
+			for {
+				q.Pop(n)
+				time.Sleep(1 * time.Second)
+			}
+		}(num)
+	}
+
+	//永远阻塞, 让main函数不退出, 让它在后台一直执行
+	select {}
+}
